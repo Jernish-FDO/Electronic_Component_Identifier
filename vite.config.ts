ea83f27d287d851d88abaf4ad1a0f91205e5b5a3
Jernish <111893985+Jernish-FDO@users.noreply.github.com>
2025-10-05 22:51:31 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
-// vite.config.ts
-
-import path from 'path';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
-    },
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
+  plugins: [react()],
+  base: '/',
+  build: {
+    outDir: 'dist',
+    chunkSizeWarningLimit: 1500, // Suppress warning
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          // Split large libraries into separate chunks
+        }
       }
     }
-});
+  }
+})
